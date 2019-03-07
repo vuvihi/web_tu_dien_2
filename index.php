66f9f2e66a7d0d496e84858a7bd51f2d624f783b
Tuong DangQuang <32568148+vuvihi@users.noreply.github.com>
2019-03-07 23:59:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,13 @@
 		<link rel="shortcut icon" href="img/icon.png">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<?php
+		$bol=!empty($_POST['bol'])?$_POST['bol']:'';
 		include 'thu_vien.php';
-$input=!empty($_POST['input'])?$_POST['input']:'';
-$start=!empty($_POST['start'])?$_POST['start']:'';
-$end=!empty($_POST['end'])?$_POST['end']:'';
+		$input=!empty($_POST['input'])?$_POST['input']:'';
+		$start=!empty($_POST['start'])?$_POST['start']:'';
+		$end=!empty($_POST['end'])?$_POST['end']:'';
 		?>
-		<title>Từ điển online</title>
+		<title>Demo Từ điển online</title>
 		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
 		<link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
@@ -35,7 +36,7 @@ $end=!empty($_POST['end'])?$_POST['end']:'';
 			<div class="container">
 				<div class="row">
 					<div class="col-md-10 col-lg-8 col-xl-7 mx-auto" style="top: -100px">
-						<form autocomplete="off" method='POST' action="index.php">
+						<form id="myform" autocomplete="off" method='POST' action="index.php">
 							<div class="form-row">
 								<div class="col-12 col-md-9 mb-2 mb-md-0">
 									<input class="form-control form-control-lg" id="input" type="text" name="input" placeholder="Nhập từ cần tra" style="border-radius: 0rem">
@@ -50,45 +51,41 @@ $end=!empty($_POST['end'])?$_POST['end']:'';
 						</form>
 					</div>
 				</div>
-				<div class="card" style="color: #fff;background-color: white;">
-					<div class="card-body">
-						<?php
-						 if($bol != '')
-          {
-            echo '<div class="card" style="color: #fff;background-color: white;">';
-            echo '<div class="card-body">';
-            if($input != '') echo '<font color="black">'.get_mean($start, $end).'</font>';
-            echo '</div></div>';
-          }
-						?>
+				<?php
+				if($bol != '')
+				{
+				echo '<div class="card" style="color: #fff;background-color: white;">';
+							echo '<div class="card-body">';
+										if($input != '') echo '<font color="black">'.get_mean($start, $end).'</font>';
+							echo '</div></div>';
+							}
+					?>
+				</div>
+			</header>
+			<!-- Footer -->
+			<footer class="footer bg-light">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+							<p class="text-muted small mb-4 mb-lg-0">&copy; A Project from 323+</p>
+						</div>
+						<div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+							<ul class="list-inline mb-0">
+								<li class="list-inline-item">
+									<a href="https://github.com/vuvihi/demotudienonline">
+										<i class="fab fa-github fa-2x fa-fw"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-		</header>
-		<!-- Footer -->
-		<footer class="footer bg-light">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-6 h-100 text-center text-lg-left my-auto">
-						<p class="text-muted small mb-4 mb-lg-0">&copy; A Project from 323+</p>
-					</div>
-					<div class="col-lg-6 h-100 text-center text-lg-right my-auto">
-						<ul class="list-inline mb-0">
-							<li class="list-inline-item">
-								<a href="https://github.com/vuvihi/demotudienonline">
-									<i class="fab fa-github fa-2x fa-fw"></i>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<!--Make sure the form has the autocomplete function switched off:-->
-		<script src="vendor/jquery/autocomplete.js"></script>
-		<script src="vendor/jquery/autocomplete_code.js"></script>
-		<!-- Bootstrap core JavaScript -->
-		<script src="vendor/jquery/jquery.min.js"></script>
-		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-	</body>
-</html>
+			</footer>
+			<!--Make sure the form has the autocomplete function switched off:-->
+			<script src="vendor/jquery/autocomplete.js"></script>
+			<script src="vendor/jquery/autocomplete_code.js"></script>
+			<!-- Bootstrap core JavaScript -->
+			<script src="vendor/jquery/jquery.min.js"></script>
+			<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		</body>
+	</html>
