1bcaedf97e6ee37a3d00b840840f32552f34be98
vuvihi <songheo29@gmail.com>
2019-03-14 00:23:46 +0700

    rollback

--- oho/jquery/autocomplete_code.js
+++ oho/jquery/autocomplete_code.js
@@ -18,9 +18,7 @@ function autocomplete(start, end, inp, arr) {
       /*for each item in the array...*/
       function index(s, e)
       {
-        max=e;
-        if((s+10)>=e) max=s+10;
-        for (i = s; i < max; i++) {
+        for (i = s; i < e; i++) {
           /*check if the item starts with the same letters as the text field value:*/
           if (arr[i][0].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
             /*create a DIV element for each matching element:*/
